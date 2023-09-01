2b77de08776696854fa4b56ce77775f1135773df
Joseph Lee <josephlee3004@gmail.com>
2023-09-01 11:37:05 -0400

    Finish phone number input validation

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 const downArrow = document.querySelector(".down-arrow-container");
+const phoneNum = document.querySelector("#phone");
+const submitBtn = document.querySelector(".submit-btn");
 
 downArrow.onclick = () => document.querySelector(".form-container").scrollIntoView();
+
+// Phone number input validation
+const constraint = new RegExp("[0-9]{10}");
+phoneNum.oninput = () => phoneNum.setCustomValidity("");
+submitBtn.onclick = () => {
+    if (constraint.test(phoneNum.value)) {
+        phoneNum.setCustomValidity("");
+    } else {
+        phoneNum.setCustomValidity("Enter a 10-digit number.");
+    }
+};
