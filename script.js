8cced0a9b5db5d8883d7e9115697193dc43b7bab
Joseph Lee <josephlee3004@gmail.com>
2023-09-01 11:47:10 -0400

    Change anonymous functions to addEventListeners

--- script.js
+++ script.js
@@ -2,15 +2,25 @@ const downArrow = document.querySelector(".down-arrow-container");
 const phoneNum = document.querySelector("#phone");
 const submitBtn = document.querySelector(".submit-btn");
 
-downArrow.onclick = () => document.querySelector(".form-container").scrollIntoView();
+downArrow.addEventListener("click", scrollToFormContainer)
+phoneNum.addEventListener("input", clearCustomValidity);
+submitBtn.addEventListener("click", validatePhoneNum);
 
-// Phone number input validation
-const constraint = new RegExp("[0-9]{10}");
-phoneNum.oninput = () => phoneNum.setCustomValidity("");
-submitBtn.onclick = () => {
+// ------------------------------ Callbacks ------------------------------
+
+function scrollToFormContainer() {
+    document.querySelector(".form-container").scrollIntoView();
+}
+
+function clearCustomValidity() {
+    this.setCustomValidity("");
+}
+
+function validatePhoneNum() {
+    const constraint = new RegExp("[0-9]{10}");
     if (constraint.test(phoneNum.value)) {
         phoneNum.setCustomValidity("");
     } else {
         phoneNum.setCustomValidity("Enter a 10-digit number.");
     }
-};
+}
